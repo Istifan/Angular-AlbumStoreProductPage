40f6cdf51e30475b2c315fa0b7d16f3633526859
Istifan <64746370+Istifan@users.noreply.github.com>
2020-10-02 11:58:27 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,20 +13,20 @@ import { Product } from './product';
 @Injectable()
 export class ProductService {
 
-  private _productsUrl = '..assets/products.json';
-
   private _albumUrl = '../assets/album.json';
 
-  constructor(private _http: Http) { }
+  private _productsUrl = '..assets/products.json';
 
-  getProducts() {
-    return this._http.get(this._productsUrl);map((response) =>
-    Observable<Product[]>response.json());
-  }
+  constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
     <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) =>
+    <Product[]>response.json());
   }
 
 }
